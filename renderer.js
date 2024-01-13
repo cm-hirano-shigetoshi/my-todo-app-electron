b5aa8fc4d33327594bbf144cc3b06c63bc8d37fd
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-01-13 21:52:31 +0900

    会議は別扱いとする

--- renderer.js
+++ renderer.js
@@ -10,6 +10,17 @@ function _timestamp(epoch, timezoneOffset = 9) {
         + `${pad(d.getUTCHours())}:${pad(d.getUTCMinutes())}:${pad(d.getUTCSeconds())}`;
 }
 
+function _epochtime(timestamp, timezoneOffset = 9) {
+    const date = new Date(timestamp);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset() + timezoneOffset * 60);
+    return date.getTime();
+}
+
+function _modifyTimestamp(timestamp, offset) {
+    const date = new Date(timestamp);
+    date.setMinutes(date.getMinutes() + offset);
+    return date.toISOString();
+}
 
 function _saveToDoList(todos) {
     ipcRenderer.invoke('save-todos', todos);
@@ -59,7 +70,10 @@ function _refreshAllTodos(todos) {
     while (todoList.firstChild) {
         todoList.removeChild(todoList.firstChild);
     }
-    for (let todo of todos.sort((a, b) => b.id - a.id)) {
+    const order = (a, b) => {
+        return _epochtime(b.tags.Date) - _epochtime(a.tags.Date)
+    }
+    for (let todo of todos.sort(order)) {
         drawTask(todo, _getUlWithText(todoList, todo.tags.Date));
     }
 }
@@ -78,71 +92,108 @@ function loadToDoList() {
 function drawTask(todo, day) {
     // ToDoのリストアイテムを作成
     const li = document.createElement('li');
-
-    const title = document.createElement('label');
-    title.textContent = todo.text;
     if (todo.id.startsWith("MTG_")) {
+        const title = document.createElement('label');
+        title.textContent = todo.text;
         title.style.background = "pink";
-    } else if (todo.done) {
-        title.style.background = "lightgreen";
-    } else if (_isRunning(todo)) {
-        title.style.fontWeight = 'bold';
-        title.style.background = "lightblue";
-    }
-    const estimateTime = document.createElement('input');
-    estimateTime.id = "estimate-time";
-    estimateTime.value = todo.estimate;
-    const measureButton = document.createElement('button');
-    if (_isRunning(todo)) {
-        measureButton.textContent = '中断';
+        const estimateTime = document.createElement('input');
+        estimateTime.id = "estimate-time";
+        estimateTime.value = todo.estimate;
+        const decreaseButton = document.createElement('button');
+        decreaseButton.textContent = "<-";
+        const timeDisplay = document.createElement('label');
+        timeDisplay.textContent = _getElapsedTime(todo);
+        const increaseButton = document.createElement('button');
+        increaseButton.textContent = "->";
+
+        // リストアイテムへのボタンの追加
+        li.appendChild(title);
+        li.appendChild(estimateTime);
+        li.appendChild(decreaseButton);
+        li.appendChild(timeDisplay);
+        li.appendChild(increaseButton);
+        day.appendChild(li);
+
+        estimateTime.addEventListener('input', function () {
+            todo.estimate = estimateTime.value;
+            _saveToDoList(todos);
+        });
+
+        decreaseButton.addEventListener('click', () => {
+            todo.times[todo.times.length - 1].end = _modifyTimestamp(todo.times[todo.times.length - 1].end, -5);
+            refresh();
+        });
+
+        increaseButton.addEventListener('click', () => {
+            todo.times[todo.times.length - 1].end = _modifyTimestamp(todo.times[todo.times.length - 1].end, 5);
+            refresh();
+        });
     } else {
-        measureButton.textContent = '開始';
-    }
-    if (todo.done) {
-        measureButton.disabled = true;
-    }
-    const timeDisplay = document.createElement('label');
-    timeDisplay.textContent = _getElapsedTime(todo);
-    const completeBtn = document.createElement('button');
-    if (todo.done) {
-        completeBtn.textContent = '取消';
-    } else {
-        completeBtn.textContent = '完了';
-    }
-
-    // リストアイテムへのボタンの追加
-    li.appendChild(title);
-    li.appendChild(estimateTime);
-    li.appendChild(measureButton);
-    li.appendChild(timeDisplay);
-    li.appendChild(completeBtn);
-    day.appendChild(li);
-
-    estimateTime.addEventListener('input', function () {
-        todo.estimate = estimateTime.value;
-        _saveToDoList(todos);
-    });
-
-    measureButton.addEventListener('click', () => {
+        const title = document.createElement('label');
+        title.textContent = todo.text;
+        if (todo.id.startsWith("MTG_")) {
+            title.style.background = "pink";
+        } else if (todo.done) {
+            title.style.background = "lightgreen";
+        } else if (_isRunning(todo)) {
+            title.style.fontWeight = 'bold';
+            title.style.background = "lightblue";
+        }
+        const estimateTime = document.createElement('input');
+        estimateTime.id = "estimate-time";
+        estimateTime.value = todo.estimate;
+        const measureButton = document.createElement('button');
         if (_isRunning(todo)) {
-            todo.times[todo.times.length - 1].end = _timestamp(Date.now());
-            refresh();
+            measureButton.textContent = '中断';
         } else {
-            todo.times.push({start: _timestamp(Date.now()), end: null});
-            refresh();
+            measureButton.textContent = '開始';
         }
-    });
-
-    completeBtn.addEventListener('click', () => {
-        todo.done = !todo.done;
         if (todo.done) {
-            if (!todo.times[todo.times.length - 1].end) {
-                // endがすでにある場合は単純に再度完了に戻すだけ
-                todo.times[todo.times.length - 1].end = _timestamp(Date.now());
-            }
+            measureButton.disabled = true;
         }
-        refresh();
-    });
+        const timeDisplay = document.createElement('label');
+        timeDisplay.textContent = _getElapsedTime(todo);
+        const completeBtn = document.createElement('button');
+        if (todo.done) {
+            completeBtn.textContent = '取消';
+        } else {
+            completeBtn.textContent = '完了';
+        }
+
+        // リストアイテムへのボタンの追加
+        li.appendChild(title);
+        li.appendChild(estimateTime);
+        li.appendChild(measureButton);
+        li.appendChild(timeDisplay);
+        li.appendChild(completeBtn);
+        day.appendChild(li);
+
+        estimateTime.addEventListener('input', function () {
+            todo.estimate = estimateTime.value;
+            _saveToDoList(todos);
+        });
+
+        measureButton.addEventListener('click', () => {
+            if (_isRunning(todo)) {
+                todo.times[todo.times.length - 1].end = _timestamp(Date.now());
+                refresh();
+            } else {
+                todo.times.push({start: _timestamp(Date.now()), end: null});
+                refresh();
+            }
+        });
+
+        completeBtn.addEventListener('click', () => {
+            todo.done = !todo.done;
+            if (todo.done) {
+                if (!todo.times[todo.times.length - 1].end) {
+                    // endがすでにある場合は単純に再度完了に戻すだけ
+                    todo.times[todo.times.length - 1].end = _timestamp(Date.now());
+                }
+            }
+            refresh();
+        });
+    }
 }
 
 function addTask(todoInput) {
